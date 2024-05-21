f8c38f40c02e139645775724f7ae9d651d194976
gbophuk0s <surta.iit1@gmail.com>
2024-05-21 20:33:01 +0300

    introduced endpoint module

--- endpoint/src/main/java/com/qulix/endpoint/EndpointApplication.java
+++ endpoint/src/main/java/com/qulix/endpoint/EndpointApplication.java
@@ -1,13 +1,13 @@
-package com.qulix.trainingtask;
+package com.qulix.endpoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainingTaskApplication {
+public class EndpointApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TrainingTaskApplication.class, args);
+        SpringApplication.run(EndpointApplication.class, args);
     }
 
 }
